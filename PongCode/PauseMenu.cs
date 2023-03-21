b46066e1658d1df7ad5dc2d178ad221b40029064
Ariana Castro <56000837+ArianaCastro01@users.noreply.github.com>
2023-03-21 14:51:50 -0400

    Update PauseMenu.cs

--- PongCode/PauseMenu.cs
+++ PongCode/PauseMenu.cs
@@ -16,36 +16,19 @@ public class PauseMenu : MonoBehaviour
         {
             if (GameIsPaused == false)
             {
-                GameIsPaused = true;
-                PauseMenuUI.SetActive(true);
-                AudioListener.pause = true; 
+                PauseMenuUI.SetActive(true); //shows the pause menu and its child objects, the panels & buttons
+                Time.timeScale = 0f;  //freezes time in the game world, effectively pausing
+                GameIsPaused = true;  //we use this boolean to track if the game is paused
             }
             
             else
             {
-                GameIsPaused = false;
-                PauseMenuUI.SetActive(false);
+                PauseMenuUI.SetActive(false); //hides the pause menu
+                Time.timeScale = 1f;  //starts time in the game world
+                GameIsPaused = tf;  //we use this boolean to track if the game is paused
             }
             
-        } 
-
-
-    }
-
-    public void Pause()
-    {
-        PauseMenuUI.SetActive(true); //shows the pause menu and its child objects, the panels & buttons
-        Time.timeScale = 0f;  //freezes time in the game world, effectively pausing
-       GameIsPaused = true;  //we use this boolean to track if the game is paused
-
-    }
-
-    public void Resume(bool tf)
-    {
-        PauseMenuUI.SetActive(false); //hides the pause menu
-
-        Time.timeScale = 1f;  //starts time in the game world
-        GameIsPaused = tf;  //we use this boolean to track if the game is paused
+        }
     }
 
     public void LoadMenu(bool tf)
